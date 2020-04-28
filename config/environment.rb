db94da23dd89b40b5b3d2b609eac1af985dc4c57
Emmanuel Nosakhare <nonreactiv@gmail.com>
2020-04-28 11:55:23 +0100

    Add mailer smtp settings

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['MAILER_USERNAME'],
+  :password => ENV['MAILER_PASSWORD'],
+  :domain => ENV['HOST'],
+  :address => ENV['MAILER_ADDRESS'],
+  :port => ENV['MAILER_PORT'],
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
