6845252e84e8edc05c583667c746a7008df7a01a
Emmanuel Nosakhare <nonreactiv@gmail.com>
2020-04-28 12:04:05 +0100

    Update enviroment

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['MAILER_USERNAME'],
   :password => ENV['MAILER_PASSWORD'],
-  :domain => ENV['HOST'],
+  :domain => ENV['DOMAIN'],
   :address => ENV['MAILER_ADDRESS'],
   :port => ENV['MAILER_PORT'],
   :authentication => :plain,
